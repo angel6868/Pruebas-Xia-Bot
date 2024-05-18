06c40d0de94c10ae1822c627877519b2226aaa61
angel6868 <149003850+angel6868@users.noreply.github.com>
2024-05-17 21:15:58 -0400

    Actualizar VF-PRUEBA-IG.js

--- plugins/VF-PRUEBA-IG.js
+++ plugins/VF-PRUEBA-IG.js
@@ -13,10 +13,7 @@ const profilePicture = await instagram.users.profilePicture();
   fs.writeFileSync('./profile_picture.jpg', profilePicture.data);
 
     const message = `Usuario: ${profile.username}\nNombre completo: ${profile.full_name}\nBiografía: ${profile.biography}\nNúmero de seguidores: ${profile.counts.followed_by}\nNúmero de publicaciones: ${profile.counts.media}\nNúmero de usuarios seguidos: ${profile.counts.follows}`;
-    conn.sendFile(m.chat, result.images.jpg.image_url, 'error.jpg', InstagramInfo, m);
-  } catch {
-    throw `*[❗] ERROR, INTENTELO DE NUEVO*`;
-  }
+getProfileInfo('<USERNAME>');
 };
 handler.command = /^(stalkig|infoig)$/i;
 export default handler;
